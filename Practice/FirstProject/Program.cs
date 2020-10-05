ca914e4dc2db490117cb4e412e8d0f62de44792f
leewallbridge <leewallbridge@hotmail.co.uk>
2020-10-05 09:40:15 +0100

    test

---
+++ Practice/FirstProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 1;
+            Console.WriteLine(number);
+            Console.WriteLine("{0} {1}",  int.MinValue,int.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+        }
+
+    } 
+}
